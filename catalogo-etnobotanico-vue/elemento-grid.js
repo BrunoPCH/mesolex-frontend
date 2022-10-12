0a3377a66b3ac2e05a7854fcc26f3f6d7b76577d
BrunoPCH <bruno.perez.ch@gmail.com>
2022-10-12 18:09:22 -0500

    Arreglo que llama a las especies

--- catalogo-etnobotanico-vue/elemento-grid.js
+++ catalogo-etnobotanico-vue/elemento-grid.js
@@ -40,4 +40,36 @@ Vue.component(
 );
 new Vue({
   el: "#vue-grid-list",
+  data() {
+    return {
+      especies: [
+        "Sambucus nigra",
+        "Amaranthus hybridus",
+        "Amaranthus spinosus",
+        "Spondias purpurea",
+        "Plumeria rubra",
+        "Bidens odorata",
+        "Porophyllum ruderale",
+        "Tagetes erecta",
+        "Crescentia cujete",
+        "Parmentiera aculeata",
+        "Tabebuia rosea",
+        "Wigandia urens",
+        "Bursera simaruba",
+        "Rhipsalis baccifera",
+        "Celtis iguanaea",
+        "Trema micrantha",
+        "Canna indica",
+        "Couepia polyandra",
+        "Licania platypus",
+        "Commelina erecta",
+        "Tradescantia zebrina",
+        "Cuscuta",
+        "Ipomoea batatas",
+        "Ipomoea indica",
+        "Merremia umbellata",
+        "Costus",
+      ],
+    };
+  },
 });
